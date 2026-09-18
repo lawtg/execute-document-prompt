f7ac2868060a4e57a8b8381b14d52294a240c278
lawtg <tgodlawakpa@gmail.com>
2026-09-19 00:01:17 +0100

    Remove Meta Pixel code

--- src/main.tsx
+++ src/main.tsx
@@ -4,25 +4,12 @@ import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
-// Meta Pixel – programmatic injection as fallback
-;(function () {
-  const win = window as any
-  if (win.fbq) return
-  const fbq: any = function (...args: any[]) {
-    fbq.callMethod ? fbq.callMethod(...args) : fbq.queue.push(args)
-  }
-  win.fbq = win._fbq = fbq
-  fbq.push = fbq
-  fbq.loaded = true
-  fbq.version = '2.0'
-  fbq.queue = []
-  const script = document.createElement('script')
-  script.async = true
-  script.src = 'https://connect.facebook.net/en_US/fbevents.js'
-  document.head.appendChild(script)
-  fbq('init', '1089854867312597')
-  fbq('track', 'PageView')
-})()
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
